ef3f43afd08c133bc45bd61c93a56d30c5230c79
Damassa <felipelealdamasceno@gmail.com>
2024-08-29 19:36:43 -0300

    refactor: colocando Typescript com React

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 import { FaInstagram, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 import MyAccountButton from '../MyAccountButton';
 
-import './style.css';
+import './style.scss';
 
 const Footer: React.FC = () => {
   return (
     <footer>
-        <div className='footer--left'>
+        <div className='footer-left'>
             Assista suas séries favoritas. <br />
             <span>World of Tokusatsu. All rights reserved.</span>
         </div>
-        <div className='footer--right'>
+        <div className='footer-right'>
             <MyAccountButton />
-            <div className='footer--social'>
+            <div className='footer-social'>
                 <a href="http://instagram.com/damassaa" target='_blank' rel="noreferrer">
-                    <FaInstagram />
+                    <FaInstagram size={20} />
                 </a>
                 <a href="http://github.com/damassa" target='_blank' rel="noreferrer">
-                    <FaGithubSquare />
+                    <FaGithubSquare size={20} />
                 </a>
                 <a href="http://linkedin.com/damassa" target='_blank' rel="noreferrer">
-                    <FaLinkedin />
+                    <FaLinkedin size={20} />
                 </a>
             </div>
         </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,29 +2,21 @@ import './style.scss';
 
 import imageBanner from '../../assets/images/undraw_horror_movie_3988.svg';
 
-import SerieCard from '../../components/SerieCard';
-import SerieList from '../../components/SerieList';
+// import SerieCard from '../../components/SerieCard';
+// import SerieList from '../../components/SerieList';
 
 const Home: React.FC = () => {
     return (
-        <>
+        <div className="page-container">
             <div className="banner">
                 <div className="banner-left">
                     <p>Assista todas as suas séries favoritas de Tokusatsu no conforto de sua casa.</p>
                 </div>
                 <div className="banner-right">
-                    <img src={imageBanner} alt="sdfsdfsd" />
+                    <img src={imageBanner} alt="imagem da homepage" />
                 </div>
             </div>
-            <div className="cards">
-                <SerieCard />
-            </div>
-            <div>
-                <SerieList />
-            </div>
-        </>
+        </div>
     );
 }
 export default Home;
-
-
