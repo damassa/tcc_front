1092274bf0a34d26e830bce8f33777dc2847989b
Damassa <felipelealdamasceno@gmail.com>
2024-08-30 17:32:16 -0300

    Adicionando Bootstrap no projeto.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,30 @@
 import './style.scss';
 
 import imageBanner from '../../assets/images/undraw_horror_movie_3988.svg';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 // import SerieCard from '../../components/SerieCard';
 // import SerieList from '../../components/SerieList';
 
 const Home: React.FC = () => {
     return (
-        <div className="page-container">
-            <div className="banner">
-                <div className="banner-left">
-                    <p>Assista todas as suas séries favoritas de Tokusatsu no conforto de sua casa.</p>
+        <>
+            <Navbar />
+                <div className="page-container">
+                    <div className="container-fluid p-5">
+                        <div className="banner">
+                            <div className="banner-left">
+                                <p>Assista todas as suas séries favoritas de Tokusatsu no conforto de sua casa.</p>
+                            </div>
+                            <div className="banner-right">
+                                <img src={imageBanner} alt="imagem da homepage" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="banner-right">
-                    <img src={imageBanner} alt="imagem da homepage" />
-                </div>
-            </div>
-        </div>
+            <Footer />
+        </>
     );
 }
 export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,38 +3,16 @@ import Home from '../pages/Home';
 import SerieDetail from '../pages/SerieDetail';
 import EditUser from '../pages/EditUser';
 import Login from '../pages/Login';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-import { useEffect, useState } from 'react';
 
 const RoutesSite: React.FC = () => {
-    const [purpleNav, setPurpleNav] = useState(false);
-
-    useEffect(() => {
-        const scrollListener = () => {
-            if(window.scrollY > 10) {
-                setPurpleNav(true);
-            } else {
-                setPurpleNav(false);
-            }
-        }
-
-        window.addEventListener('scroll', scrollListener);
-        return () => {
-            window.removeEventListener('scroll', scrollListener);
-        }
-    }, []);
-    
     return (
         <BrowserRouter>
-            <Navbar purple = {purpleNav} />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path='/detail' element={<SerieDetail />} />
                 <Route path='/edit' element={<EditUser />} />
                 <Route path='/login' element={<Login />} />
             </Routes>
-            <Footer />
         </BrowserRouter>
     );
 }
